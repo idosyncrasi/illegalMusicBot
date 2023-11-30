1aee6249503a89f06cbbec38614cd95696c38930
idosyncrasi <28535876+idosyncrasi@users.noreply.github.com>
2023-11-29 20:27:33 -0500

    improvement: dont steal my bot token

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const data = {
 	'prefix': '!', // Prefix bot looks for to run commands
-	'token': "MTA4ODIxODQ2MDcyODg2NDg4OQ.G9OwLD.4dIetyvMH9SgW3ekoAIljv_27xxib1ZsRzuRH0" // Discord bot token
+	'token': "" // Discord bot token
 }
